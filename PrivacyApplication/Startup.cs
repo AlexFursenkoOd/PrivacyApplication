892456b7fa55410d4e4844fad08b7f44407e7ebf
AlexFursenkoOd <andersen.od.ua@gmail.com>
2018-07-31 11:22:49 +0300

    Initial commit

---
+++ PrivacyApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrivacyApplication.Startup))]
+namespace PrivacyApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
